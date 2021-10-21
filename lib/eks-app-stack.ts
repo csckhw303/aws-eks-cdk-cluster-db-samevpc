4b8221724b109e98890182828642b0801c901f23
Seok Chan Chung <csckhw@gmail.com>
2021-10-20 21:03:38 -0400

    initial

--- lib/eks-app-stack.ts
+++ lib/eks-app-stack.ts
@@ -1,9 +1,12 @@
-import * as cdk from '@aws-cdk/core';
+import * as cdk from "@aws-cdk/core";
+import * as eks from "@aws-cdk/aws-eks";
 
 export class EksAppStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-
+    const cluster = new eks.Cluster(this, "cdk-eks-cluster", {
+      version: eks.KubernetesVersion.V1_18,
+    });
     // The code that defines your stack goes here
   }
 }
